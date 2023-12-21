f58b7c302c0a65303fc02b173041855fac6dcc87
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2023-12-21 12:42:54 +0530

    removed focus to input

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { useRef, useEffect } from "react";
 
-function TodoForm({ addData, editID ,input,inputData}) {
-  const inputRef = useRef("null");
-  useEffect(() => {
-    inputRef.current.focus(); //used to focus on refrence
-  });
+function TodoForm({ addData, editID, input, inputData }) {
   return (
     <form className="form" onSubmit={(e) => e.preventDefault()}>
       <input
         className="form-text"
         type="text"
         value={input}
-        ref={inputRef}
         placeholder={editID ? " Updating ToDo..." : "Enter Your ToDo"}
         onChange={inputData}
       />
